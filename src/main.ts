1ecb8687cfd17b69ce6e62d32275584bb1ba0dc9
jhchoi123 <solid2113@naver.com>
2023-05-13 01:27:07 +0900

    refactor build config to es module

--- src/main.ts
+++ src/main.ts
@@ -13,63 +13,61 @@ import { commands as slashCommands, handleSlashCommand } from "./command/slash";
 import { closeDB, connectDB, isDbExist } from "./db";
 import { initDB } from "./db/init";
 
-(async () => {
-    // load .env file
-    config();
+// load .env file
+config();
 
-    if (!process.env.DISCORD_TOKEN || process.env.DISCORD_TOKEN == "") {
-        console.error("Invalid discord bot token.");
-        process.exit(1);
+if (!process.env.DISCORD_TOKEN || process.env.DISCORD_TOKEN == "") {
+    console.error("Invalid discord bot token.");
+    process.exit(1);
+}
+
+if (!(await isDbExist())) {
+    await initDB();
+}
+
+process.on("exit", async () => {
+    await closeDB();
+});
+
+const client = new Client({
+    intents: [
+        IntentsBitField.Flags.Guilds,
+        IntentsBitField.Flags.GuildMessages,
+        IntentsBitField.Flags.MessageContent
+    ]
+});
+
+const rest = new REST();
+rest.setToken(process.env.DISCORD_TOKEN);
+
+client.on("ready", async (c) => {
+    console.log(`Bot logged in as ${c.user.tag} (ID: ${c.user.id})`);
+
+    const commands: Command[] = [...slashCommands];
+    await registerCommands(rest, {
+        clientId: c.application.id,
+        commands
+    });
+
+    await connectDB();
+});
+
+client.on("interactionCreate", async (interaction) => {
+    if (interaction.isChatInputCommand()) {
+        const i = interaction as ChatInputCommandInteraction;
+        await handleSlashCommand(i);
     }
-
-    if (!(await isDbExist())) {
-        await initDB();
+    if (interaction.isUserContextMenuCommand()) {
+        const i = interaction as UserContextMenuCommandInteraction;
     }
-
-    process.on("exit", async () => {
-        await closeDB();
-    });
-
-    const client = new Client({
-        intents: [
-            IntentsBitField.Flags.Guilds,
-            IntentsBitField.Flags.GuildMessages,
-            IntentsBitField.Flags.MessageContent
-        ]
-    });
-
-    const rest = new REST();
-    rest.setToken(process.env.DISCORD_TOKEN);
-
-    client.on("ready", async (c) => {
-        console.log(`Bot logged in as ${c.user.tag} (ID: ${c.user.id})`);
-
-        const commands: Command[] = [...slashCommands];
-        await registerCommands(rest, {
-            clientId: c.application.id,
-            commands
-        });
-
-        await connectDB();
-    });
-
-    client.on("interactionCreate", async (interaction) => {
-        if (interaction.isChatInputCommand()) {
-            const i = interaction as ChatInputCommandInteraction;
-            await handleSlashCommand(i);
-        }
-        if (interaction.isUserContextMenuCommand()) {
-            const i = interaction as UserContextMenuCommandInteraction;
-        }
-        if (interaction.isMessageContextMenuCommand()) {
-            const i = interaction as MessageContextMenuCommandInteraction;
-        }
-    });
-
-    try {
-        await client.login(process.env.DISCORD_TOKEN);
-    } catch (e) {
-        console.error(e);
-        process.exit(1);
+    if (interaction.isMessageContextMenuCommand()) {
+        const i = interaction as MessageContextMenuCommandInteraction;
     }
-})();
+});
+
+try {
+    await client.login(process.env.DISCORD_TOKEN);
+} catch (e) {
+    console.error(e);
+    process.exit(1);
+}
--- src/util/path.ts
+++ src/util/path.ts
@@ -1,8 +1,9 @@
-import { resolve } from "path";
+import { dirname, resolve } from "path";
+import { fileURLToPath } from "url";
 
-const CURRENT_PATH = __dirname;
+const DIRNAME = dirname(fileURLToPath(import.meta.url));
 
-const SRC_ROOT = resolve(CURRENT_PATH, "..");
+const SRC_ROOT = resolve(DIRNAME, "..");
 const PROJECT_ROOT = resolve(SRC_ROOT, "..");
 
 export { SRC_ROOT, PROJECT_ROOT, resolve };
