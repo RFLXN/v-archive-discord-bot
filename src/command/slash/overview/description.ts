315d5d0d26d4189c5f149293fe24d64abf26c3e1
jhchoi123 <solid2113@naver.com>
2023-05-13 17:20:39 +0900

    fix for thread reply

--- src/command/slash/overview/description.ts
+++ src/command/slash/overview/description.ts
@@ -4,50 +4,50 @@ import { Score } from "../../../db/entity/score";
 const createEmbedDescription = (username: string, totalPatterns: number, scores: Score[]) => {
     const sorted = scores
         .sort((a, b) => {
-            if (!b.pattern.floor && !a.pattern.floor) {
+            if (!b?.pattern?.floor && !a?.pattern?.floor) {
                 return b.pattern.level - a.pattern.level;
             }
-            return (b.pattern.floor ?? 0) - (a.pattern.floor ?? 0);
+            return (b?.pattern?.floor ?? 0) - (a?.pattern?.floor ?? 0);
         });
 
     const totalPerfectScores = scores
         .filter((score) => score.score == 100);
-    const highestPerfect = totalPerfectScores[0]?.pattern.floor
+    const highestPerfect = totalPerfectScores[0]?.pattern?.floor
         ? `Floor: ${totalPerfectScores[0].pattern.floor}` : totalPerfectScores[0]?.pattern.level
             ? `Level: ${totalPerfectScores[0].pattern.level}` : undefined;
     const totalPerfect = totalPerfectScores.length;
 
     const totalMaxComboScores = scores.filter((score) => score.fullCombo == 1);
-    const highestMaxCombo = totalMaxComboScores[0]?.pattern.floor
+    const highestMaxCombo = totalMaxComboScores[0]?.pattern?.floor
         ? `Floor: ${totalMaxComboScores[0].pattern.floor}` : totalMaxComboScores[0]?.pattern.level
             ? `Level: ${totalMaxComboScores[0].pattern.level}` : undefined;
     const totalMaxCombo = totalMaxComboScores.length;
 
     const totalOver999Scores = scores.filter((score) => score.score >= 99.9);
-    const highestOver999 = totalOver999Scores[0]?.pattern.floor
+    const highestOver999 = totalOver999Scores[0]?.pattern?.floor
         ? `Floor: ${totalOver999Scores[0].pattern.floor}` : totalOver999Scores[0]?.pattern.level
             ? `Level: ${totalOver999Scores[0].pattern.level}` : undefined;
     const totalOver999 = totalOver999Scores.length;
 
     const totalOver995Scores = scores.filter((score) => score.score >= 99.5);
-    const highestOver995 = totalOver995Scores[0]?.pattern.floor
+    const highestOver995 = totalOver995Scores[0]?.pattern?.floor
         ? `Floor: ${totalOver995Scores[0].pattern.floor}` : totalOver995Scores[0]?.pattern.level
             ? `Level: ${totalOver995Scores[0].pattern.level}` : undefined;
     const totalOver995 = totalOver995Scores.length;
 
     const totalOver99Scores = scores.filter((score) => score.score >= 99);
-    const highestOver99 = totalOver99Scores[0]?.pattern.floor
+    const highestOver99 = totalOver99Scores[0]?.pattern?.floor
         ? `Floor: ${totalOver99Scores[0].pattern.floor}` : totalOver99Scores[0]?.pattern.level
             ? `Level: ${totalOver99Scores[0].pattern.level}` : undefined;
     const totalOver99 = totalOver99Scores.length;
 
     const totalOver97Scores = scores.filter((score) => score.score >= 97);
-    const highestOver97 = totalOver97Scores[0]?.pattern.floor
+    const highestOver97 = totalOver97Scores[0]?.pattern?.floor
         ? `Floor: ${totalOver97Scores[0].pattern.floor}` : totalOver97Scores[0]?.pattern.level
             ? `Level: ${totalOver97Scores[0].pattern.level}` : undefined;
     const totalOver97 = totalOver97Scores.length;
 
-    const highestClear = sorted[0]?.pattern.floor
+    const highestClear = sorted[0]?.pattern?.floor
         ? `Floor: ${sorted[0].pattern.floor}` : sorted[0]?.pattern.level
             ? `Level: ${sorted[0].pattern.level}` : undefined;
     const totalClear = sorted.length;
